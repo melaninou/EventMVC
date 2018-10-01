1239d2744c6afefbc07b4a006e7a720b80d858c5
Iris Nael <irnael@ttu.ee>
2018-10-01 16:17:26 +0300

    RAW - Controllers(Event, Home, Profile), Views (CreateEvent, ViewEvents, ViewProfiles)

---
+++ EventProject/Controllers/EventController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventProject.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Edit(string id)
+        {
+            return View();
+        }
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+        public ActionResult Delete(string id)
+        {
+            return View();
+        }
+    }
+}
