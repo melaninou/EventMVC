cfe2a1d2bfded3498e9928e60eff9e631c1a62c2
Iris Nael <irnael@ttu.ee>
2018-10-01 14:52:13 +0300

    Raw view (create event, view events ja view profile)

--- EventProject/Controllers/HomeController.cs
+++ EventProject/Controllers/HomeController.cs
@@ -33,7 +33,24 @@ namespace EventProject.Controllers
         {
             return View();
         }
+        public IActionResult CreateEvent()
+        {
+            ViewData["Message"] = "Your application description page.";
 
+            return View();
+        }
+        public IActionResult ViewEvents()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+        public IActionResult ViewProfile()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
