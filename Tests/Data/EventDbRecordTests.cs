cea6e2a8c1fb537d7d3d806d76244d63c74f2f00
Darja Molodtsova <damolo@ttu.ee>
2018-10-03 18:29:07 +0300

    Tests

--- Tests/Data/EventDbRecordTests.cs
+++ Tests/Data/EventDbRecordTests.cs
@@ -4,18 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests.Data
 {
+    [TestClass]
     public class EventDbRecordTests :ObjectTests<EventDbRecord>
     {
         protected override EventDbRecord getRandomTestObject()
         {
             return GetRandom.Object<EventDbRecord>();
-        }
-
-        [TestMethod]
-        public void IDTest()
-        {
-            testReadWriteProperty(() => obj.ID, x => obj.ID = x);
-            testNullEmptyAndWhitespaceCases(() => obj.ID, x => obj.ID = x, () => obj.ID);
         }
     }
 }
---
+++ Tests/Data/IsDataTested.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Data
+{
+    [TestClass]
+    public class IsDataTested :AssemblyTests
+    {
+        private const string assembly = "Data";
+        protected override string Namespace(string name)
+        {
+            return $"{name}";
+        }
+
+        [TestMethod]
+        public void IsCommonTested()
+        {
+            isAllClassesTested(assembly, Namespace("Common"));
+        }
+    }
+}
